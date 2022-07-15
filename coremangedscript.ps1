748421340affc0a7ac1d960104fc31099105069c
amasimov <32998194+amasimov@users.noreply.github.com>
2022-07-15 11:00:38 +0100

    Update coremangedscript.ps1

--- coremangedscript.ps1
+++ coremangedscript.ps1
@@ -1,3 +1,3 @@
 Install-WindowsFeature AD-Domain-Services
 Import-Module ADDSDeployment
-Install-ADDSForest -DomainName coremanged.co.uk -Confirm -DomainNetbiosName COREMANAGED -Force -InstallDns -LogPath C:\\Windows\NTDS -SafeModeAdministratorPassword (ConvertTo-SecureString -String "p@ssw0rd" -AsPlainText -Force) -SysvolPath C:\\Windows\SYSVOL
+Install-ADDSForest -DomainName coremanaged.co.uk -Confirm -DomainNetbiosName COREMANAGED -Force -InstallDns -LogPath C:\\Windows\NTDS -SafeModeAdministratorPassword (ConvertTo-SecureString -String "p@ssw0rd" -AsPlainText -Force) -SysvolPath C:\\Windows\SYSVOL
